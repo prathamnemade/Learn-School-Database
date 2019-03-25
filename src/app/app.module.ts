9e22adfdcce7c56ab9a909a90425aed3bb16360b
prathamnemade <prathameshnemade95@gmail.com>
2019-03-25 21:44:16 +0530

    UI skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MainScreenComponent } from './main-screen/main-screen.component';
+import { LoginComponent } from './main-screen/login/login.component';
+import { RegisterComponent } from './main-screen/register/register.component';
+import { SubMainComponent } from './sub-main/sub-main.component';
+import { ViewComponent } from './sub-main/view/view.component';
+import { AddComponent } from './sub-main/add/add.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainScreenComponent,
+    LoginComponent,
+    RegisterComponent,
+    SubMainComponent,
+    ViewComponent,
+    AddComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,BrowserAnimationsModule,ReactiveFormsModule,HttpClientModule,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
